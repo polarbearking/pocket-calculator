729bf9cd34d72063fa2aef4c5f1344452fa9c5c8
Charles McCloud <coolmantackle13@gmail.com>
2019-01-07 10:49:59 -0500

    W.I.P

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,40 @@
+let x
+let y
+let answer
 function zero() {
-  document.getElementById("display").innerHTML= "0"
+  x = "0" + x
+}
+function one() {
+  x = "1" + x
+}
+function two() {
+  x = "2" + x
+}
+function three() {
+  x = "3" + x
+}
+function four() {
+  x = "4" + x
+}
+function five() {
+  x = "5" + x
+}
+function six() {
+  x = "6" + x
+}
+function seven() {
+  x = "7" + x
+}
+function eight() {
+  x = "8" + x
+}
+function nine() {
+  x = "9" + x
+}
+function add() {
+  answer = x + y
+}
+function consoleDisplay() {
+  console.log(answer)
+  console.log(x)
 }
